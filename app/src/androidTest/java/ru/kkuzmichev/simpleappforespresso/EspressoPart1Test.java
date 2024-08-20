837a979259eb9a3ac2199651a435bf197e1543ba
Evgen7985 <Evgen7985@yandex.ru>
2024-08-20 15:16:06 +0500

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EspressoPart1Test.java
@@ -0,0 +1,41 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@LargeTest
+//@RunWith(AndroidJUnit4.class)
+@RunWith(AllureAndroidJUnit4.class)
+public class EspressoPart1Test {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction textView = onView(withId(R.id.text_home));
+        textView.check(matches(isDisplayed()));
+        textView.check(matches(withText("This is home fragment")));
+    }
+}
